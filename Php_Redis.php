44bfd1e9bc37a889a8b5ad3e9fe977476cf2c243
zhouhg <996472676@qq.com>
2022-03-29 09:38:37 +0800

    init

---
+++ Php_Redis.php
@@ -0,0 +1,15 @@
+<?php
+class Php_Redis{
+    private $ip,$port,$password;
+    public function __construct($ip,$port,$password){
+        $this->ip = $ip;
+        $this->port = $port;
+        $this->password = $password;
+    }
+
+    public function getConnect(){
+        $redis = new Redis();
+        $redis->connect($this->ip,$this->port);
+        echo "Connection to server successfully";
+    }
+}
---
+++ Test.php
@@ -0,0 +1,7 @@
+<?php
+$predis = new Php_Redis('127.0.0.1','6379','');
+$predis->getConnect();
+
+
+
+?>
